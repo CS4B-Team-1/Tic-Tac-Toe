079bccea18d19b9c6a03bdfd1a31a1cf44119292
Mitch Merrell <themitchguy@gmail.com>
2026-01-24 19:38:01 -0800

    Update TicTacToe.java
    now loads the stage with the board for testing

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,17 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TicTacToeBoard.fxml"));
+        AnchorPane boardAnchorPane = loader.<AnchorPane>load();
 
+        primaryStage.setScene(new Scene(boardAnchorPane));
+        primaryStage.show();
     }
 
     public static void main(String[] args) throws Exception {
