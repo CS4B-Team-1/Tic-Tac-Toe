69fd52e06571a94d22159d43ccdc5660d9e5bb70
Mitch Merrell <themitchguy@gmail.com>
2026-02-01 21:02:46 -0800

    updated the Computer class functions

--- Computer.java
+++ Computer.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 
 public class Computer {
     private ArrayList<Integer> boardPosition;
-    private boolean isMaximizer;
+    private boolean isComputerMaximizer;
 
-    public Computer(ArrayList<Integer> boardPosition, boolean isMaximizer) {
-        
+    private static final int MINIMIZER_VALUE = -1;
+    private static final int MAXIMIZER_VALUE = 1;
+
+    public Computer(ArrayList<Integer> boardPosition, boolean isComputerMaximizer) {
         this.boardPosition = boardPosition;
-        this.isMaximizer = isMaximizer;
+        this.isComputerMaximizer = isComputerMaximizer;
     }
 
     // Loops through the board list, and returns all indices where the
@@ -21,14 +23,14 @@ public class Computer {
         return moves;
     }
 
-    public void getBestMove() {
+    public int getBestMove() {
         int bestScore = Integer.MIN_VALUE;
-        int bestMove;
+        int bestMove = -1;
         int moveValue;
-        if (this.isMaximizer)
-            moveValue = 1;
+        if (this.isComputerMaximizer)
+            moveValue = MAXIMIZER_VALUE;
         else
-            moveValue = -1;
+            moveValue = MINIMIZER_VALUE;
 
         // for each available move, do the following:
             // make the move on the board
@@ -38,13 +40,21 @@ public class Computer {
         for (Integer move: this.availableMoves()) {
             // set the move on the board
             this.boardPosition.set(move, moveValue);
-            // call minimax
-            int evaluatedScore = minimax();
+            // call minimax with depth 0 and the maximizer/minimizer value of the Computer
+            int evaluatedScore = minimax(0, this.isComputerMaximizer); // returns a score evaluation of the move
+            // undo the move (reset the array representation of the move to "empty" -> 0)
             this.boardPosition.set(move, 0);
+            // if the score is better than the best score, update the best move and best score
+            if ((evaluatedScore * moveValue) > bestScore) {  // multiply the score by the maximizer/minimizer value here to always return a positive value for ease of evaluation
+                bestScore = evaluatedScore;
+                bestMove = move;
+            }
         }
+        // returns the index of the best move to make
+        return bestMove;
     }
 
-    public int minimax() {
+    public int minimax(int depth, boolean isMaximizer) {
         return 0;
     }
 }
